cfc3bae4fa8e2bd5b44fec1e9194f9896e62ba0d
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2024-03-25 12:04:06 +0100

    updated

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aaliyyah/phpiggy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8482f6ddd870b080bde520c8262c8407dce48d3b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aaliyyah/phpiggy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8482f6ddd870b080bde520c8262c8407dce48d3b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
